d0602535abe13b7886900f4e66ff927436adce49
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2022-11-29 10:47:13 +0100

    Update klanten_pagina.php

--- klanten_pagina.php
+++ klanten_pagina.php
@@ -6,5 +6,22 @@
     </head>
     <body>
         <p>test</p>
+        <?php 
+        
+           // Create connection
+           $conn = mysqli_connect($servername, $username, $password, $dbname);
+           // Check connection
+           if (!$conn) {
+           die("Connection failed: " . mysqli_connect_error());
+           }
+
+           $sql = "SELECT * FROM inloggen"
+           $result = mysqli_query($conn, $sql);
+
+           if (mysqli_num_rows($result) > 0){
+            // output data of each row 
+           }
+
+           ?> 
     </body>
 </html>
