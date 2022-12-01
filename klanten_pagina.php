5e5e0409e94e260c1ecb285cdc6585faf69bd50f
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2022-12-01 13:55:14 +0100

    Update klanten_pagina.php

--- klanten_pagina.php
+++ klanten_pagina.php
@@ -5,14 +5,12 @@
         </style>
     </head>
     <body>
-        <p>test</p>
         <?php
 
         $servername = "localhost";
         $username = "root";
         $password = "";
         $dbname = "Bestellen.db";
-        
         ?>
         <?php 
     
@@ -28,35 +26,13 @@
         if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-            echo  " - Naam: " . $row["Naam"]. " -Telefoonnummer: " . $row["Telefoonnummer"]. " - Postcode: " . $row["Postcode"]. " - Huisnummer: " . $row["Huisnummer"]. "<br>";
+            echo  " - Naam: " . $row["Naam"]. " -Telefoonnummer: " . $row["Telefoonnummer"]. " - Email:  " . $row["Email"].  " - Postcode: " . $row["Postcode"]. " - Huisnummer: " . $row["Huisnummer"]. "<br>";
         }
         } else {
         echo "0 results";
         }
 
         mysqli_close($conn);
-
-                        // Create connection
-                        $conn = mysqli_connect($servername, $username, $password, $dbname);
-                        // Check connection
-                        if (!$conn) {
-                        die("Connection failed: " . mysqli_connect_error());
-                        }
-        
-                        $sql = "SELECT * FROM inloggen";
-                        $result = mysqli_query($conn, $sql);
-        
-                        if (mysqli_num_rows($result) > 0) {
-                        // output data of each row
-                        while($row = mysqli_fetch_assoc($result)) {
-                            echo "id: " . $row["ID"]. "- email " . $row["email"]. "<br>";
-                        }
-                        } else {
-                        echo "0 results";
-                        }
-        
-                        mysqli_close($conn);
-                        ?>
 
            ?> 
     </body>
